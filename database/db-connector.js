ea856524d8b390fa0f31056cafad97cba1c0bafb
patbc <culleyp@oregonstate.edu>
2023-03-25 00:06:26 -0500

    updated handlebars and db=connector files

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,11 +1,3 @@
-/*
-Citation Scope: Creating and establishing a connection from app.js to the DB  
-Date: 3/9/2023
-Originality: Copied from step 1 of 'Connecting to a MySQL Database.'  
-Source: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%201%20-%20Connecting%20to%20a%20MySQL%20Database
-*/
-// ./database/db-connector.js
-
 // Require an SQL instance to be used for DB connection 
 var mysql = require('mysql')
 
